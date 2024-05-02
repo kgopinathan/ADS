60acc0789ed4f6d4064c42dd3712e874586c58b2
kgopinathan <102571694+kgopinathan@users.noreply.github.com>
2024-05-02 12:28:12 +0530

    Add files via upload

---
+++ InsertStatements.sql
@@ -0,0 +1,22 @@
+insert into "dbo"."XXX" values ('a', 'a')
+GO
+insert into "dbo"."XXX" values ('aa', 'aa')
+GO
+insert into "dbo"."XXX" values ('aaa', 'aaa')
+GO
+insert into "dbo"."XXX" values ('aaaa', 'aaaa')
+GO
+insert into "dbo"."XXX" values ('aaaaa', 'aaaaa')
+GO
+insert into "dbo"."XXX" values ('aaaaaa', 'aaaaaa')
+GO
+insert into "dbo"."XXX" values ('aaaaaaa', 'aaaaaaa')
+GO
+insert into "dbo"."XXX" values ('aaaaaaaa', 'aaaaaaaa')
+GO
+insert into "dbo"."XXX" values ('aaaaaaaaa', 'aaaaaaaa')
+GO
+insert into "dbo"."XXX" values ('aaaaaaaaaa', 'aaaaaaaa')
+GO
+insert into "dbo"."XXX" values ('aaaaaaaaaaa', 'aaaaaaaaa')
+GO
---
+++ UpdateStatements.sql
@@ -0,0 +1,3 @@
+update "dbo"."XXX" set A1 = 'BB' where A2 = 'a'
+go
+update "dbo"."XXX" set A1 = 'BB' where A2 = 'aa'
